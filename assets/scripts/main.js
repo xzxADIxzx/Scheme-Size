4b2cc884aa9d071bc73064272b3688e62e3cdfc1
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-08-03 19:56:35 +0300

    ...

---
+++ assets/scripts/main.js
@@ -0,0 +1,4 @@
+var meta = Vars.mods.locateMod("justmod").meta;
+meta.author = "[#0096FF]xzxADIxzx";
+meta.displayName = "Scheme Size";
+meta.description = "";
---
+++ src/example/ExampleJavaMod.java
@@ -0,0 +1,32 @@
+package example;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class ExampleJavaMod extends Mod{
+
+    public ExampleJavaMod(){
+        Log.info("Loaded ExampleJavaMod constructor.");
+
+        Events.on(ClientLoadEvent.class, e -> {
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("frog");
+                dialog.cont.add("behold").row();
+                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
+    }
+
+    @Override
+    public void loadContent(){
+		Log.info("Loading some example content.");
+    }
+
+}
