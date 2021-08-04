c18146d841e9bab925f3429de85530a00859470f
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-08-04 10:29:14 +0300

    ...

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-var meta = Vars.mods.locateMod("justmod").meta;
+var meta = Vars.mods.locateMod("scheme-size").meta;
 meta.author = "[#0096FF]xzxADIxzx";
 meta.displayName = "Scheme Size";
 meta.description = "";
