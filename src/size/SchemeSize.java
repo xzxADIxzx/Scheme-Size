a0f5d17d02a3361d1bc50c3d02f5f14a1f98f35d
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-08-04 12:58:00 +0300

    ...

--- src/size/SchemeSize.java
+++ src/size/SchemeSize.java
@@ -2,59 +2,7 @@ package size;
 
 import mindustry.mod.*;
 import mindustry.Vars;
-import mindustry.game.*;
-import mindustry.game.Schematics.*;
-
-// import arc.*;
-// import arc.util.*;
-import mindustry.ui.dialogs.*;
-// import mindustry.*;
-// import mindustry.content.*;
-// import mindustry.game.EventType.*;
-// import mindustry.gen.*;
-// import mindustry.input.*;
-// import mindustry.game.schematic.*;
-// import mindustry.world.*;
-
-import arc.*;
-import arc.assets.*;
-import arc.files.*;
-import arc.graphics.*;
-import arc.graphics.g2d.*;
-import arc.graphics.gl.*;
-import arc.math.*;
-import arc.math.geom.*;
-import arc.struct.*;
-import arc.util.*;
-import arc.util.io.*;
-import arc.util.io.Streams.*;
-import arc.util.pooling.*;
-import arc.util.serialization.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.core.*;
-import mindustry.ctype.*;
-import mindustry.entities.units.*;
-import mindustry.game.EventType.*;
-import mindustry.game.Schematic.*;
-import mindustry.gen.*;
-import mindustry.input.*;
-import mindustry.input.Placement.*;
-import mindustry.io.*;
-import mindustry.world.*;
-import mindustry.world.blocks.ConstructBlock.*;
-import mindustry.world.blocks.distribution.*;
-import mindustry.world.blocks.legacy.*;
-import mindustry.world.blocks.power.*;
-import mindustry.world.blocks.production.*;
-import mindustry.world.blocks.sandbox.*;
-import mindustry.world.blocks.storage.*;
-import mindustry.world.meta.*;
-
-import java.io.*;
-import java.util.zip.*;
-
-// import static mindustry.Vars.*;
+import size.Schematics;
 
 public class SchemeSize extends Mod{
 
@@ -69,73 +17,9 @@ public class SchemeSize extends Mod{
         });
     }
 
-    // public Schematic create64(int x, int y, int x2, int y2){
-    //     NormalizeResult result = Placement.normalizeArea(x, y, x2, y2, 0, false, 64);
-    //     x = result.x;
-    //     y = result.y;
-    //     x2 = result.x2;
-    //     y2 = result.y2;
-
-    //     int ox = x, oy = y, ox2 = x2, oy2 = y2;
-
-    //     Seq<Stile> tiles = new Seq<>();
-
-    //     int minx = x2, miny = y2, maxx = x, maxy = y;
-    //     boolean found = false;
-    //     for(int cx = x; cx <= x2; cx++){
-    //         for(int cy = y; cy <= y2; cy++){
-    //             Building linked = world.build(cx, cy);
-    //             Block realBlock = linked == null ? null : linked instanceof ConstructBuild cons ? cons.current : linked.block;
-
-    //             if(linked != null && realBlock != null && (realBlock.isVisible() || realBlock instanceof CoreBlock)){
-    //                 int top = realBlock.size/2;
-    //                 int bot = realBlock.size % 2 == 1 ? -realBlock.size/2 : -(realBlock.size - 1)/2;
-    //                 minx = Math.min(linked.tileX() + bot, minx);
-    //                 miny = Math.min(linked.tileY() + bot, miny);
-    //                 maxx = Math.max(linked.tileX() + top, maxx);
-    //                 maxy = Math.max(linked.tileY() + top, maxy);
-    //                 found = true;
-    //             }
-    //         }
-    //     }
-
-    //     if(found){
-    //         x = minx;
-    //         y = miny;
-    //         x2 = maxx;
-    //         y2 = maxy;
-    //     }else{
-    //         return new Schematic(new Seq<>(), new StringMap(), 1, 1);
-    //     }
-
-    //     int width = x2 - x + 1, height = y2 - y + 1;
-    //     int offsetX = -x, offsetY = -y;
-    //     IntSet counted = new IntSet();
-    //     for(int cx = ox; cx <= ox2; cx++){
-    //         for(int cy = oy; cy <= oy2; cy++){
-    //             Building tile = world.build(cx, cy);
-    //             Block realBlock = tile == null ? null : tile instanceof ConstructBuild cons ? cons.current : tile.block;
-
-    //             if(tile != null && !counted.contains(tile.pos()) && realBlock != null
-    //                 && (realBlock.isVisible() || realBlock instanceof CoreBlock)){
-    //                 Object config = tile instanceof ConstructBuild cons ? cons.lastConfig : tile.config();
-
-    //                 tiles.add(new Stile(realBlock, tile.tileX() + offsetX, tile.tileY() + offsetY, config, (byte)tile.rotation));
-    //                 counted.add(tile.pos());
-    //             }
-    //         }
-    //     }
-
-    //     return new Schematic(tiles, new StringMap(), width, height);
-    // }
-
-    public Schematic createTEST(int x, int y, int x2, int y2){
-        return null;
-    }
-
     @Override
     public void loadContent(){
-        Vars.schematics.create = createTESTNEW(int x, int y, int x2, int y2) => {return null;};
+        Vars.schematics = new Schematics();
     }
 
 }
