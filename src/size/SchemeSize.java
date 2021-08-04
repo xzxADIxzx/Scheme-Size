43600cecd4ee6ab48b0e6593ac6b1b9bf8b7370f
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-08-04 14:18:30 +0300

    ...

--- src/size/SchemeSize.java
+++ src/size/SchemeSize.java
@@ -15,14 +15,7 @@ import mindustry.ui.dialogs.*;
 public class SchemeSize extends Mod{
 
     public SchemeSize(){
-        Events.on(ClientLoadEvent.class, e -> {
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
+        
     }
 
     @Override
