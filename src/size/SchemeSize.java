ae029077c86950a33471a40067ee49a48eeaf2e0
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-08-04 16:39:15 +0300

    ...

--- src/size/SchemeSize.java
+++ src/size/SchemeSize.java
@@ -14,12 +14,11 @@ import mindustry.ui.dialogs.*;
 public class SchemeSize extends Mod{
 
     public SchemeSize(){
-        
+        Events.on(ClientLoadEvent.class, e -> {
+            //wait 10 secs, because... idk
+            Time.runTask(10f, () -> {
+                Vars.schematics = new Schematics64();
+            });
+        });
     }
-
-    @Override
-    public void loadContent(){
-        Vars.schematics = new Schematics64();
-    }
-
 }
