f73818a16c828ab91d922377fcba6b8c20cb5e88
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-08-04 11:56:14 +0300

    ...

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -8,14 +8,12 @@ import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
-
-import mindustry.Vars;
+import mindustry.input.*;
+import mindustry.game.schematic.*;
 
 public class ExampleJavaMod extends Mod{
 
     public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
         Events.on(ClientLoadEvent.class, e -> {
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
@@ -26,9 +24,69 @@ public class ExampleJavaMod extends Mod{
         });
     }
 
+    public Schematic create64(int x, int y, int x2, int y2){
+        NormalizeResult result = Placement.normalizeArea(x, y, x2, y2, 0, false, 64);
+        x = result.x;
+        y = result.y;
+        x2 = result.x2;
+        y2 = result.y2;
+
+        int ox = x, oy = y, ox2 = x2, oy2 = y2;
+
+        Seq<Stile> tiles = new Seq<>();
+
+        int minx = x2, miny = y2, maxx = x, maxy = y;
+        boolean found = false;
+        for(int cx = x; cx <= x2; cx++){
+            for(int cy = y; cy <= y2; cy++){
+                Building linked = world.build(cx, cy);
+                Block realBlock = linked == null ? null : linked instanceof ConstructBuild cons ? cons.current : linked.block;
+
+                if(linked != null && realBlock != null && (realBlock.isVisible() || realBlock instanceof CoreBlock)){
+                    int top = realBlock.size/2;
+                    int bot = realBlock.size % 2 == 1 ? -realBlock.size/2 : -(realBlock.size - 1)/2;
+                    minx = Math.min(linked.tileX() + bot, minx);
+                    miny = Math.min(linked.tileY() + bot, miny);
+                    maxx = Math.max(linked.tileX() + top, maxx);
+                    maxy = Math.max(linked.tileY() + top, maxy);
+                    found = true;
+                }
+            }
+        }
+
+        if(found){
+            x = minx;
+            y = miny;
+            x2 = maxx;
+            y2 = maxy;
+        }else{
+            return new Schematic(new Seq<>(), new StringMap(), 1, 1);
+        }
+
+        int width = x2 - x + 1, height = y2 - y + 1;
+        int offsetX = -x, offsetY = -y;
+        IntSet counted = new IntSet();
+        for(int cx = ox; cx <= ox2; cx++){
+            for(int cy = oy; cy <= oy2; cy++){
+                Building tile = world.build(cx, cy);
+                Block realBlock = tile == null ? null : tile instanceof ConstructBuild cons ? cons.current : tile.block;
+
+                if(tile != null && !counted.contains(tile.pos()) && realBlock != null
+                    && (realBlock.isVisible() || realBlock instanceof CoreBlock)){
+                    Object config = tile instanceof ConstructBuild cons ? cons.lastConfig : tile.config();
+
+                    tiles.add(new Stile(realBlock, tile.tileX() + offsetX, tile.tileY() + offsetY, config, (byte)tile.rotation));
+                    counted.add(tile.pos());
+                }
+            }
+        }
+
+        return new Schematic(tiles, new StringMap(), width, height);
+    }
+
     @Override
     public void loadContent(){
-		Log.info("Loading some example content.");
+        Vars.schematics.create = create64()
     }
 
 }
