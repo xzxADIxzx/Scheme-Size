be919add08bb89dc3deab5bf9c29cba9c699d06a
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-08-05 23:19:32 +0300

    ...

--- src/input/DesktopInput512.java
+++ src/input/DesktopInput512.java
@@ -44,8 +44,8 @@ public class DesktopInput512 extends DesktopInput{
 
             // Show Size
             Placement.NormalizeDrawResult normalized = Placement.normalizeDrawArea(block, schemX, schemY, cursorX, cursorY, false, 512, 1);
-            int sizeX = normalized.x = normalized.x2;
-            int sizeY = normalized.y = normalized.y2;
+            int sizeX = normalized.x - normalized.x2;
+            int sizeY = normalized.y - normalized.y2;
             String info = Integer.toString(sizeX) + ", " + Integer.toString(sizeY);
             ui.showLabel(info, 1, cursorX * 10, cursorY * 10);
         }
