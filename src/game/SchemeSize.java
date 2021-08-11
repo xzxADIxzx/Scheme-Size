1ba1b69bd6d10803e0f4c901e4b0457d413cc351
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-08-11 15:46:46 +0300

    ...

--- src/game/SchemeSize.java
+++ src/game/SchemeSize.java
@@ -24,7 +24,7 @@ public class SchemeSize extends Mod{
 
                 // Add Settings
                 var game = Vars.ui.settings.game;
-                game.sliderPref("justspace", 0, 0, 0, 0, i -> i + "");
+                game.sliderPref("justspace", 1, 1, 1, 1, i -> i + "");
                 game.sliderPref("copysize", 512, 32, 512, 32, i -> Core.bundle.format("setting.blocks", i));
                 game.sliderPref("breaksize", 512, 32, 512, 32, i -> Core.bundle.format("setting.blocks", i));
                 game.checkPref("copyshow", true);
