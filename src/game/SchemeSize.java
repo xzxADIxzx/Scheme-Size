d18c258d21404017a57b0d5403da875621c3dab8
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-08-17 17:32:10 +0300

    ...

--- src/game/SchemeSize.java
+++ src/game/SchemeSize.java
@@ -36,7 +36,7 @@ public class SchemeSize extends Mod{
 
                 // Add Zoom Scale
                 Slider sliderMax = game.getCells().get(12).<Stack>get().getChildren().get(0);
-                Slider sliderMin = game.getCells().get(13).(Stack)get().getChildren().get(0);
+                Slider sliderMin = game.getCells().get(13).<Stack>get().getChildren().get(0);
                 sliderMax.changed(() -> { Vars.renderer.maxZoom = sliderMax.getValue() / 4f * 6f; });
                 sliderMin.changed(() -> { Vars.renderer.minZoom = sliderMin.getValue() / 4f * 1.5f; });
 
