34f05bbfdef5b767001a70fab387177a1df6b150
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-08-17 15:15:17 +0300

    ...

--- src/game/SchemeSize.java
+++ src/game/SchemeSize.java
@@ -25,14 +25,19 @@ public class SchemeSize extends Mod{
                 // Add Settings
                 var game = Vars.ui.settings.game;
                 game.sliderPref("justspace", 1, 1, 1, 1, i -> i + "");
-                game.sliderPref("maxzoommul", 1, .25f, 2, 0.25f, i -> i + "x");
-                game.sliderPref("minzoommul", 1, .25f, 2, 0.25f, i -> i + "x");
+                game.sliderPref("maxzoommul", 100, 25, 200, 25, i -> i / 100 + "x");
+                game.sliderPref("minzoommul", 100, 25, 200, 25, i -> i / 100 + "x");
                 game.sliderPref("copysize", 512, 32, 512, 32, i -> Core.bundle.format("setting.blocks", i));
                 game.sliderPref("breaksize", 512, 32, 512, 32, i -> Core.bundle.format("setting.blocks", i));
                 game.checkPref("copyshow", true);
                 game.checkPref("breakshow", true);
                 game.getCells().get(11).visible(false); // Hide justspace
-                game.getCells().get(12).changed(() -> { Vars.renderer.maxZoom = 6 * slider.GetValue(); });
+
+                // Add Zoom Scale
+                Slider slider = game.getCells().get(12).slider;
+                slider.changed(() -> { Vars.renderer.maxZoom = 6 * slider.GetValue() / 100; });
+                slider = game.getCells().get(13).slider;
+                slider.changed(() -> { Vars.renderer.maxZoom = 6 * slider.GetValue() / 100; });
 
                 // Add Logs
                 // Log.info(Vars.schematics);
