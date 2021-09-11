0cb25e492160c9f3b00edef1099a4920e94a0b54
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-09-11 19:00:17 +0300

    ...

--- src/game/SchemeSize.java
+++ src/game/SchemeSize.java
@@ -3,8 +3,10 @@ package mindustry.game;
 import arc.*;
 import arc.util.*;
 import arc.input.*;
+import arc.input.InputDevice.*;
 import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
+import arc.KeyBinds.*;
 import mindustry.mod.*;
 import mindustry.Vars;
 import mindustry.game.EventType.*;
@@ -47,11 +49,12 @@ public class SchemeSize extends Mod{
                 Vars.renderer.minZoom = 1f / (sliderMin.getValue() / 4f) * 1.5f;
 
                 // Add keybind
-                var keybinds = new Array();
-                Binding.values().forEach(item -> keybinds.splice(0, 0, item));
-                keybinds.reverse();
-                keybinds.splice(3, 0, new Bind());
-                Core.keybinds.setDefaults(keybinds);
+                // var keybinds = new Array();
+                // Binding.values().forEach(item -> keybinds.splice(0, 0, item));
+                // keybinds.reverse();
+                // keybinds.splice(3, 0, new Bind());
+                // Core.keybinds.setDefaults(keybinds);
+                Core.keybinds.setDefaults(ExBinding.values());
 
                 // Add logs
                 // Log.info(Vars.schematics);
@@ -60,11 +63,96 @@ public class SchemeSize extends Mod{
         });
     }
 
-    public class Bind implements KeyBind {
-        
+    public enum ExBinding implements KeyBind{
+        move_x(new Axis(KeyCode.a, KeyCode.d), "general"),
+        move_y(new Axis(KeyCode.s, KeyCode.w)),
+        mouse_move(KeyCode.mouseBack),
+        pan(KeyCode.mouseForward),
+
+        boost(KeyCode.shiftLeft),
+        control(KeyCode.controlLeft),
+        respawn(KeyCode.v),
+        select(KeyCode.mouseLeft),
+        deselect(KeyCode.mouseRight),
+        break_block(KeyCode.mouseRight),
+
+        pickupCargo(KeyCode.leftBracket),
+        dropCargo(KeyCode.rightBracket),
+
+        command(KeyCode.g),
+
+        clear_building(KeyCode.q),
+        pause_building(KeyCode.e),
+        rotate(new Axis(KeyCode.scroll)),
+        rotateplaced(KeyCode.r),
+        diagonal_placement(KeyCode.controlLeft),
+        pick(KeyCode.mouseMiddle),
+
+        schematic_select(KeyCode.f),
+        schematic_flip_x(KeyCode.z),
+        schematic_flip_y(KeyCode.x),
+        schematic_menu(KeyCode.t),
+
+        category_prev(KeyCode.comma, "blocks"),
+        category_next(KeyCode.period),
+
+        block_select_left(KeyCode.left),
+        block_select_right(KeyCode.right),
+        block_select_up(KeyCode.up),
+        block_select_down(KeyCode.down),
+        block_select_01(KeyCode.num1),
+        block_select_02(KeyCode.num2),
+        block_select_03(KeyCode.num3),
+        block_select_04(KeyCode.num4),
+        block_select_05(KeyCode.num5),
+        block_select_06(KeyCode.num6),
+        block_select_07(KeyCode.num7),
+        block_select_08(KeyCode.num8),
+        block_select_09(KeyCode.num9),
+        block_select_10(KeyCode.num0),
+
+        zoom(new Axis(KeyCode.scroll), "view"),
+        menu(Core.app.isAndroid() ? KeyCode.back : KeyCode.escape),
+        fullscreen(KeyCode.f11),
+        pause(KeyCode.space),
+        minimap(KeyCode.m),
+        research(KeyCode.b),
+        planet_map(KeyCode.n),
+        block_info(KeyCode.f1),
+        toggle_menus(KeyCode.c),
+        screenshot(KeyCode.p),
+        toggle_power_lines(KeyCode.f5),
+        toggle_block_status(KeyCode.f6),
+        toggle_display_items(KeyCode.f7),
+        player_list(KeyCode.tab, "multiplayer"),
+        chat(KeyCode.enter),
+        chat_history_prev(KeyCode.up),
+        chat_history_next(KeyCode.down),
+        chat_scroll(new Axis(KeyCode.scroll)),
+        chat_mode(KeyCode.tab),
+        console(KeyCode.f8),
+        ;
+
+        private final KeybindValue defaultValue;
+        private final String category;
+
+        Binding(KeybindValue defaultValue, String category){
+            this.defaultValue = defaultValue;
+            this.category = category;
+        }
+
+        Binding(KeybindValue defaultValue){
+            this(defaultValue, null);
+        }
+
         @Override
         public KeybindValue defaultValue(DeviceType type){
-            return KeyCode.backslash;
+            return defaultValue;
+        }
+
+        @Override
+        public String category(){
+            return category;
         }
     }
 }
