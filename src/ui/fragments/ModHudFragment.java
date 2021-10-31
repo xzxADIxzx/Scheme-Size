1bae38ff1246fcd211d1c9483daa5500a5bd68e6
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-10-31 21:14:15 +0300

    ...

--- src/ui/fragments/ModHudFragment.java
+++ src/ui/fragments/ModHudFragment.java
@@ -351,7 +351,7 @@ public class ModHudFragment extends Fragment{
                     mode.button(Icon.defense, check, () -> bt.setMode(Mode.wall)).checked(t -> bt.mode == Mode.wall).name("wall").row();
                 }).row();
             }).height(254f).visible(() -> shownBT && shown && !ui.minimapfrag.shown()).update(t -> {
-                if(block != null) t.setTranslation(-block.getWidth(), 0);
+                if(block != null) t.setTranslation(-block.getWidth() + Scl.slc(4), 0);
             });
         });
     }
