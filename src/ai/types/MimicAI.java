bbc330a6073b5f73352955d188055e3cceed9250
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-11-15 16:51:15 +0300

    ...

--- src/ai/types/MimicAI.java
+++ src/ai/types/MimicAI.java
@@ -16,10 +16,10 @@ public class MimicAI extends AIController{
     @Override
     public void updateMovement(){
         if(following != null){
-            moveTo(following, following.hitSize() / 2f * 1.1f + unit.hitSize / 2f + 15f, 50f);
+            // moveTo(following, following.hitSize() / 2f * 1.1f + unit.hitSize / 2f + 15f, 50f);
 
-            unit.aim(following.aimX(), following.aimY());
-            unit.controlWeapons(true, following.isShooting);
+            // unit.aim(following.aimX(), following.aimY());
+            // unit.controlWeapons(true, following.isShooting);
 
             unit.plans.clear();
             unit.plans.addFirst(following.buildPlan());
