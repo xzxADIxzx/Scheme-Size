fc3d9f6edfaf48445781cdff57b3870b27ccc80b
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-07-22 19:22:27 +0300

    ...

---
+++ src/resources/scripts/main.js
@@ -0,0 +1,11 @@
+// well, after the 136th build, it became much easier
+Vars.maxSchematicSize = 511;
+
+// it is really useful for development
+var  mod = Vars.mods.getMod("scheme-size-new")
+var get = (pkg) => mod.loader.loadClass(pkg).newInstance()
+
+const SchemeMain = mod.main
+const SchemeVars = get("scheme.SchemeVars")
+const SchemeUpdater = get("scheme.SchemeUpdater")
+const Backdoor = get("scheme.Backdoor")
