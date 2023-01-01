c9a369dc1819b165cd38364163a8740ce8a3eedd
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-01-01 23:06:48 +0300

    add the ability to change claj server address

--- src/java/scheme/ui/dialogs/ManageRoomsDialog.java
+++ src/java/scheme/ui/dialogs/ManageRoomsDialog.java
@@ -3,6 +3,7 @@ package scheme.ui.dialogs;
 import arc.graphics.Color;
 import arc.net.Client;
 import arc.scene.ui.layout.Table;
+import arc.util.Strings;
 import mindustry.gen.Icon;
 import mindustry.gen.Tex;
 import mindustry.ui.Styles;
@@ -16,29 +17,35 @@ import java.io.IOException;
 
 public class ManageRoomsDialog extends BaseDialog {
 
-    public static final String serverIP = "darkdustry.net";
-    public static final int serverPort = 10000;
+    public String serverIP = "darkdustry.net";
+    public int serverPort = 3917;
 
     public ManageRoomsDialog() {
         super("@manage.name");
         addCloseButton();
 
-        Table list = new Table();
-        list.defaults().width(750f).padBottom(8f);
-
-        shown(() -> list.getCells().filter(cell -> cell.get() != null));
-
         cont.table(rooms -> {
             rooms.defaults().width(750f);
 
+            Table list = new Table();
+            list.defaults().width(750f).padBottom(8f);
+            list.update(() -> list.getCells().filter(cell -> cell.get() != null));
             rooms.add(list).row();
+
+            rooms.field("darkdustry.net:3917", address -> {
+                int semicolon = address.indexOf(':');
+
+                serverIP = address.substring(0, semicolon);
+                serverPort = Strings.parseInt(address.substring(semicolon + 1));
+            }).maxTextLength(100).valid(address -> address.contains(":")).row();
+
             rooms.button("@manage.create", () -> {
                 try {
                     list.add(new Room()).row();
                 } catch (Throwable ignored) {
                     ui.showErrorMessage(ignored.getMessage());
                 }
-            }).disabled(button -> list.getChildren().size >= 8);
+            }).disabled(button -> list.getChildren().size >= 8).padTop(8f);
         }).height(550f).row();
 
         cont.labelWrap("@manage.tooltip").labelAlign(2, 8).padTop(16f).width(400f).get().getStyle().fontColor = Color.lightGray;
@@ -55,7 +62,7 @@ public class ManageRoomsDialog extends BaseDialog {
         }
 
         root.row();
-        root.button("@manage.name", Icon.planet, this::show).colspan(2).width(450).disabled(button -> !net.server()).row();
+        root.button("@manage.name", Icon.planet, this::show).colspan(2).width(450f).disabled(button -> !net.server()).row();
 
         int index = state.isCampaign() || state.isEditor() ? 5 : 7;
         root.getCells().insert(index, root.getCells().remove(index + 1));
