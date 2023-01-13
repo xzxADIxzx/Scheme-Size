e59bf21ba8ec53e0cc0b11aebf02e493a8c15e87
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-01-13 23:02:40 +0300

    add display selection

--- src/java/scheme/ui/dialogs/ImageParserDialog.java
+++ src/java/scheme/ui/dialogs/ImageParserDialog.java
@@ -1,16 +1,22 @@
 package scheme.ui.dialogs;
 
 import arc.files.Fi;
+import arc.scene.style.TextureRegionDrawable;
+import arc.scene.ui.ImageButton.ImageButtonStyle;
 import arc.scene.ui.layout.Table;
 import mindustry.game.Schematic;
+import mindustry.gen.Tex;
+import mindustry.ui.Styles;
 import mindustry.ui.dialogs.BaseDialog;
 import mindustry.ui.dialogs.SchematicsDialog.SchematicImage;
+import mindustry.world.blocks.logic.LogicDisplay;
 import scheme.SchemeVars;
 import scheme.tools.ImageParser;
 import scheme.tools.ImageParser.Config;
 import scheme.ui.TextSlider;
 
 import static arc.Core.*;
+import static mindustry.Vars.*;
 
 public class ImageParserDialog extends BaseDialog {
 
@@ -24,28 +30,41 @@ public class ImageParserDialog extends BaseDialog {
         super("@parser.name");
         addCloseButton();
 
-        cont.defaults().width(450f);
         cont.table(t -> image = t).row();
 
         new TextSlider(1f, 10f, 1f, 1f, value -> {
             app.post(this::rebuild);
             return bundle.format("parser.row", config.rows = value);
-        }).build(cont).row();
+        }).build(cont).width(450f).row();
 
         new TextSlider(1f, 10f, 1f, 1f, value -> {
             app.post(this::rebuild);
             return bundle.format("parser.column", config.columns = value);
-        }).build(cont).row();
+        }).build(cont).width(450f).row();
 
         cont.check("@parser.filter", true, value -> {
             app.post(this::rebuild);
             config.filter = value;
-        }).width(0f).left();
+        }).left().row();
 
         buttons.button("@parser.import", () -> {
             SchemeVars.schemas.imported(last);
             hide();
         }).disabled(button -> last == null);
+
+        cont.table(table -> {
+            var style = new ImageButtonStyle(Styles.clearNoneTogglei) {{
+                up = Tex.pane;
+            }};
+
+            content.blocks().each(block -> {
+                if (block instanceof LogicDisplay display)
+                    table.button(new TextureRegionDrawable(display.uiIcon), style, 30f, () -> {
+                        app.post(this::rebuild);
+                        config.display = display;
+                    }).size(48f).padRight(4f).checked(button -> config.display == display);
+            });
+        }).left().row();
     }
 
     public void rebuild() {
