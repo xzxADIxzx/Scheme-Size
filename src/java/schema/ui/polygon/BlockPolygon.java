04269056c20ed02a358ae5b7f64816063d0e9a2e
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-03-07 19:10:12 +0300

    add a list of blocks to the block polygon

--- src/java/schema/ui/polygon/BlockPolygon.java
+++ src/java/schema/ui/polygon/BlockPolygon.java
@@ -1,15 +1,23 @@
 package schema.ui.polygon;
 
+import arc.func.*;
+import arc.graphics.*;
 import arc.scene.*;
+import arc.scene.style.*;
+import arc.scene.ui.layout.*;
 import arc.util.*;
 import mindustry.gen.*;
 import mindustry.type.*;
 import mindustry.world.*;
+import schema.ui.Style;
 
+import static arc.Core.*;
 import static mindustry.Vars.*;
+import static schema.Main.*;
 
 import static mindustry.type.Category.*;
 import static mindustry.type.Category.logic;
+import static mindustry.type.Category.units;
 
 /** Polygon that displays the block selection wheel. */
 public class BlockPolygon extends Polygon {
@@ -28,8 +36,43 @@ public class BlockPolygon extends Polygon {
         super.build(parent);
         for (int i = 0; i < all.length; i++) add(String.valueOf(icons[i]), true, j -> {
             draw = false;
+
+            add(new Table(atlas.drawable("schema-panel"), list -> {
+                list.margin(12f);
+                list.defaults().pad(4f);
+
+                // TODO top part of the list
+
+                list.pane(Style.scr, pane -> {
+                    pane.margin(0f, 4f, 4f, 8f);
+                    pane.defaults().pad(0f, -4f, -4f, 0f);
+
+                    eachUnlocked(categories[j], b -> {
+                        var core = player.core();
+                        var available = player.isBuilder() && (state.rules.infiniteResources || (core != null && core.items.has(b.requirements)));
+
+                        var button = pane.button(new TextureRegionDrawable(b.uiIcon), Style.ibc, () -> {
+
+                            if (input.shift() || input.ctrl() || input.alt())
+                                copy(icon(b.name) + "");
+                            else
+                                insys.block = b;
+
+                            list.remove();
+                            hide();
+
+                        }).size(48f).checked(insys.block == b && available).disabled(available).get();
+
+                        button.resizeImage(32f);
+                        button.image().color(!b.isPlaceable() ? Color.darkGray : !available ? Color.gray : Color.white);
+                    });
+                });
+            }));
         });
     }
+
+    @Override
+    public void select() { if (draw) super.select(); }
 
     /** Whether the given block is unlocked. */
     public boolean unlocked(Block block) { return block.placeablePlayer && block.unlockedNow() && block.environmentBuildable(); }
