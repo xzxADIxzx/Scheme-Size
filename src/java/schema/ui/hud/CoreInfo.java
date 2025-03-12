27f668382654f56e9d0d7cb2452fb7c8592653c7
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-03-12 21:49:54 +0300

    add core items display

--- src/java/schema/ui/hud/CoreInfo.java
+++ src/java/schema/ui/hud/CoreInfo.java
@@ -8,9 +8,11 @@ import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.type.*;
 import mindustry.world.modules.*;
+import schema.input.*;
 import schema.ui.*;
 
 import static mindustry.Vars.*;
+import static schema.Main.*;
 
 /** Subfragment that displays the core items, their flow and power grids. */
 public class CoreInfo extends Table {
@@ -51,10 +53,29 @@ public class CoreInfo extends Table {
         clearChildren();
         table(cont -> {
 
-        });
+            cont.table(t -> content.items().each(used::contains, i -> {
+
+                t.top().left();
+
+                t.image(i.uiIcon).size(24f);
+                t.label(() -> core == null
+                    ? "[disabled]-"
+                    : Keybind.display_prod.down()
+                        ? formatFlow(core.getFlowRate(i))
+                        : format(core.get(i), false)
+                ).minWidth(80f).padLeft(4f).left();
+
+                if (t.getChildren().size % width == 0) t.row();
+
+            })).growY().width(4f * (24f + 4f + 80f)).padBottom(8f).row();
+
+        }).growY();
         table(btns -> {
             btns.button(Style.icon(team), Style.ibt, 40f, () -> chooseTeam = !chooseTeam).size(48f).padBottom(8f).row();
             btns.button(Icon.menu,        Style.ibc,      () -> chooseGrid = !chooseGrid).size(48f);
         }).top();
     }
+
+    /** Formats the given number as a flow. */
+    public String formatFlow(float num) { return (num > 0 ? "[green]+" : num < 0 ? "[scarlet]" : "") + format(num, true) + "[light]/s"; }
 }
