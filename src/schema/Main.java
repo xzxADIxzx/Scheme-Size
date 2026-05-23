9283d81ee563e982bfd33318350c17c57113d5dc
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2026-05-23 19:28:08 +0300

    separate loader and tools

---
+++ src/schema/Main.java
@@ -0,0 +1,27 @@
+package schema;
+
+import arc.struct.*;
+import mindustry.mod.*;
+
+/// Main class of the mod that loads, initializes and stores different components of it.
+public class Main extends Mod
+{
+    // region components
+
+    /// List of fetched servers that host CLaJ.
+    public static Seq<String> clajURLs;
+    /// List of events acquired via reflection.
+    public static ObjectMap<?, Seq<?>> events;
+
+    // endregion
+
+    /// Loads content such as tools, dialogs, fragments and so on.
+    public void load() { }
+
+    @Override
+    public void init()
+    {
+        Tools.log("[green]=> Loading content...");
+        load();
+    }
+}
---
+++ src/schema/Tools.java
@@ -0,0 +1,62 @@
+package schema;
+
+import arc.*;
+import arc.util.*;
+import mindustry.ui.*;
+
+import static arc.Core.*;
+import static mindustry.Vars.*;
+import static schema.Main.*;
+
+/// Set of different tools for simplifying life and systematization of code.
+public final class Tools
+{
+    /// Prints the given info into the dev console.
+    public static void log(String info) { Log.infoTag("Schema", info); }
+
+    /// Prints the given error into the dev console.
+    public static void err(Throwable e) { Log.errTag("Schema", Strings.getStackTrace(e)); }
+
+    /// Returns the icon's character by its name.
+    public static char icon(String name) { return (char) Fonts.getUnicode(name); }
+
+    /// Copies the given text into the clipboard.
+    public static void copy(String text)
+    {
+        app.setClipboardText(text);
+        ui.showInfoFade("@copied");
+    }
+
+    /// Use this **extremely carefully** as it clears all event listeners created by the given class.
+    public static void clear(Class<?> target)
+    {
+        if (events == null)
+            events = Reflect.get(Events.class, "events");
+
+        int count = 0;
+        for (var pair : events) count += pair.value.size - pair.value.removeAll(l -> l.toString().startsWith(target.getName())).size;
+
+        log("Cleared [red]" + count + "[] events of " + target.getSimpleName());
+    }
+
+    /// Returns the given number with a fixed number of decimal places.
+    public static String format(float num, boolean flow)
+    {
+        if (num >= 100_000_000_000f)
+            return Strings.fixed(num / 1_000_000_000f, 0) + "[light]b";
+        if (num >= 1_000_000_000f)
+            return Strings.fixed(num / 1_000_000_000f, 1) + "[light]b";
+
+        if (num >= 100_000_000f)
+            return Strings.fixed(num / 1_000_000f, 0) + "[light]m";
+        if (num >= 1_000_000f)
+            return Strings.fixed(num / 1_000_000f, 1) + "[light]m";
+
+        if (num >= 100_000f)
+            return Strings.fixed(num / 1_000f, 0) + "[light]k";
+        if (num >= 1_000f)
+            return Strings.fixed(num / 1_000f, 1) + "[light]k";
+
+        return Strings.fixed(num, flow ? 1 : 0);
+    }
+}
