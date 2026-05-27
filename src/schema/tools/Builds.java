8e059e948e4c5e7f03c8ec9b1c49eb2f75ac4113
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2026-05-27 22:12:58 +0300

    update the builds tool

--- src/schema/tools/Builds.java
+++ src/schema/tools/Builds.java
@@ -10,26 +10,27 @@ import mindustry.world.blocks.distribution.ItemBridge.*;
 
 import static mindustry.Vars.*;
 
-/** Utility class that helps iterate buildings. */
-public class Builds {
-
-    /** List of buildings that was iterated. */
+/// Utility focused on buildings.
+public class Builds
+{
+    /// List of buildings that were iterated.
     private Seq<Building> iterated = new Seq<>();
-    /** List of visible tiles acquired via reflection. Contains only the tiles with buildings or boulders on them. */
+    /// List of visible tiles with buildings.
     private Seq<Tile> tiles;
 
-    /** Clears the list of iterated buildings. Call it before iterating anything. */
+    /// Clears the list of iterated buildings.
     public void clearIterated() { iterated.clear(); }
 
-    /** Iterates visible tiles with buildings on them. */
-    public void iterateBuilds(Cons<Tile> cons) {
+    /// Iterates visible tiles with buildings.
+    public void iterateBuilds(Cons<Tile> cons)
+    {
         if (tiles == null) tiles = Reflect.get(renderer.blocks, "tileview");
         tiles.each(t -> t.build != null, cons);
     }
 
-    /** Iterates tiles around the given core and connected storages. */
-    public void iterateCore(Building build, Cons2<Tile, Integer> cons) {
-
+    /// Iterates tiles around the given core and connected storages.
+    public void iterateCore(Building build, Cons2<Tile, Integer> cons)
+    {
         iterated.add(build);
         build.proximity.each(p -> p.items == build.items && !iterated.contains(p), p -> iterateCore(p, cons));
 
@@ -38,8 +39,8 @@ public class Builds {
             min = (build.block.size - 1) / 2,
             max = (build.block.size / 2);
 
-        for (int i = -min; i <= max; i++) { // there used to be complex logic here, but it was simplified
-
+        for (int i = -min; i <= max; i++)
+        {
             cons.get(world.tile(bdx + i, bdy + max + 1), 1); // top
             cons.get(world.tile(bdx + i, bdy - min - 1), 3); // bottom
 
@@ -48,10 +49,9 @@ public class Builds {
         }
     }
 
-    /** Iterates the given and consequentially connected bridges. */
-    public void iterateBridge(ItemBridgeBuild build, Cons<ItemBridgeBuild> cons) {
-
-        // limit the amount of bridges to iterate
+    /// Iterates the given bridge and consequentially connected ones.
+    public void iterateBridge(ItemBridgeBuild build, Cons<ItemBridgeBuild> cons)
+    {
         if (iterated.add(build).size > 16) return;
 
         var linked = world.build(build.link);
@@ -60,18 +60,18 @@ public class Builds {
         cons.get(build);
     }
 
-    /** Calculates some values needed to draw a health bar for the given building. */
-    public void healthBar(Building build, float radius, boolean inner, Cons4<Float, Float, Float, Float> context) {
-
+    /// Calculates a bunch of values required to draw a health bar.
+    public void healthBar(Building build, float radius, boolean outer, Cons4<Float, Float, Float, Float> context)
+    {
         // single block builds have smaller health and status bars
         float multiplier = build.block.size > 1 ? 1f : .64f;
 
-        // the radius is taken from status indicator, but it is rotated by 45 degrees
+        // the radius is taken from the status indicator, but needs to rotated by 45 degrees
         radius *= multiplier * Mathf.sqrt2;
 
         // padding from the edges of the building
         float padding = 4f * multiplier - radius;
 
-        context.get(inner ? radius + multiplier * (Mathf.sqrt2 - 1) : radius, build.hitSize() / 2f - padding, build.x, build.y - build.hitSize() / 2f + padding + radius);
+        context.get(outer ? radius : radius + multiplier * (Mathf.sqrt2 - 1), build.hitSize() / 2f - padding, build.x, build.y - build.hitSize() / 2f + padding + radius);
     }
 }
