6946937a3adf997b855258f84aa8787e4d4c1202
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2026-06-01 13:58:00 +0300

    update the hud fragment

--- src/schema/ui/fragments/HudFragment.java
+++ src/schema/ui/fragments/HudFragment.java
@@ -1,11 +1,20 @@
 package schema.ui.fragments;
 
+import arc.*;
+import arc.graphics.*;
+import arc.math.*;
 import arc.scene.*;
+import arc.scene.event.*;
 import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
 import arc.util.*;
+import mindustry.game.*;
+import mindustry.gen.*;
 import schema.ui.*;
 import schema.ui.hud.*;
+
+import static mindustry.Vars.*;
+import static schema.Main.*;
 
 /// Fragment that displays the core and power grid info, controlled unit and its configuration, minimap and wave info.
 public class HudFragment extends Table
@@ -25,10 +34,17 @@ public class HudFragment extends Table
     /// Builds the fragment and overrides the original.
     public void build(Group parent)
     {
+        Events.run(EventType.Trigger.teamCoreDamage, () -> show(Notification.cores, 3f));
+
         parent.addChild(this);
 
         setFillParent(true);
-        visible(() -> shown);
+        update(() ->
+        {
+            if (state.isPaused()) show(netServer.isWaitingForPlayers() ? Notification.waits : Notification.pause, 0f);
+            if (!insys.building) show(Notification.build, 0f);
+        }
+        ).visible(() -> shown);
 
         top().add(unit, core, wave);
         row();
@@ -39,8 +55,17 @@ public class HudFragment extends Table
             for (var type : Notification.all) cont.collapser(c ->
             {
                 nots[type.ordinal()] = c.background(Style.find("panel-h-shape")).add("").style(Style.outline).get();
+
+                if (type != Notification.cores) return;
+
+                c.parent.tapped(() -> insys.poseCam(Tmp.v1.set(control.lastDamagedCore)));
+                c.parent.addListener(new HandCursorListener());
+                c.parent.update(() ->
+                {
+                    nots[type.ordinal()].color.set(Color.orange).lerp(Color.scarlet, Mathf.absin(2f, 1f));
+                });
             },
-            true, () -> Time.time < time[type.ordinal()]).row();
+            true, () -> Time.time <= time[type.ordinal()]).row();
         }
         ).colspan(3);
 
@@ -51,14 +76,14 @@ public class HudFragment extends Table
         core.build();
         // wave.build();
 
-        // TODO paused/pause disabled/waiting for players/saving notifications
-        // TODO leave drop point warning?
         // TODO agent: setHudText, toggleHudText, showToast, hasToast, showUnlock
     }
 
     /// Shows a notification for the given duration.
     public void show(Notification not, String sectors, float duration)
     {
+        if (duration != 0f && Time.time >= time[not.ordinal()]) Sounds.uiNotify.play();
+
         nots[not.ordinal()].setText(not.format(sectors));
         time[not.ordinal()] = Time.time + duration * 60f;
     }
