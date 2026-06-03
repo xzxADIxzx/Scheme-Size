ee31ffd8bbee0b38fcd22ada0ad31ed145ec53fd
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2026-06-03 18:01:33 +0300

    update the hud fragment

--- src/schema/ui/fragments/HudFragment.java
+++ src/schema/ui/fragments/HudFragment.java
@@ -45,6 +45,9 @@ public class HudFragment extends Table
         {
             nextGuardian(w -> show(Notification.guardian, String.valueOf(w), 5f));
         });
+        Events.on(SectorCaptureEvent .class, e -> show(Notification.captured, e.sector.name(), 5f));
+        Events.on(SectorInvasionEvent.class, e -> show(Notification.attacked, e.sector.name(), 5f));
+        Events.on(SectorLoseEvent    .class, e -> show(Notification.lost,     e.sector.name(), 5f));
 
         parent.addChild(this);
 
