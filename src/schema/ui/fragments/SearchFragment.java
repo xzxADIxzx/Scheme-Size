8357ceae2d3261064e43329f92688a29447e6d6a
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2026-06-10 15:10:08 +0300

    add a search engine

--- src/schema/ui/fragments/SearchFragment.java
+++ src/schema/ui/fragments/SearchFragment.java
@@ -6,9 +6,14 @@ import arc.scene.actions.*;
 import arc.scene.event.*;
 import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
+import arc.struct.*;
 import arc.util.*;
+import mindustry.world.*;
 import schema.input.*;
 import schema.ui.*;
+
+import static mindustry.Vars.*;
+import static schema.Main.*;
 
 /// Fragment that displays a search bar.
 public class SearchFragment extends Table
@@ -17,6 +22,11 @@ public class SearchFragment extends Table
     private TextField field;
     /// Search results.
     private Table results;
+
+    /// Ranked list of blocks.
+    private Seq<Block> ranked = new Seq<>();
+    /// Selected ranked block.
+    private int selected;
 
     public SearchFragment() { touchable = Touchable.enabled; }
 
@@ -86,7 +96,11 @@ public class SearchFragment extends Table
         if(value.matches("[0-9 () \\.\\+\\-\\*\\/]*"))
         {
             var num = evaluate(value, 0);
-            results.add(Float.isNaN(num) ? "@search.syntax-flaw" : "[light]= " + num).style(Style.outline);
+            results.add(Float.isNaN(num) ? "@search.syntax-flaw" : "[light]= " + Strings.autoFixed(num, 4)).style(Style.outline);
+        }
+        else
+        {
+            rank(value);
         }
     }
 
@@ -178,8 +192,55 @@ public class SearchFragment extends Table
         },
     };
 
+    /// Matrix required by the Levenshtein distance algorithm.
+    private static final byte[][] m = new byte[128][128];
+
+    /// Returns the case-independent biased Levenshtein distance with position-independent bias and minimal allocations.
+    public static float distance(String inA, String inB)
+    {
+        if (inA == null || inA.isBlank() || inA.length() > 127 ||
+            inB == null || inB.isBlank() || inB.length() > 127) return Float.POSITIVE_INFINITY;
+
+        var a = inA.toLowerCase(java.util.Locale.ROOT);
+        var b = inB.toLowerCase(java.util.Locale.ROOT);
+
+        var lnA = a.length();
+        var lnB = b.length();
+
+        for (byte x = 0; x <= lnA; x++)
+        for (byte y = 0; y <= lnB; y++)
+        {
+            if (x == 0) m[x][y] = y;
+            else
+            if (y == 0) m[x][y] = x;
+            else
+                m[x][y] = (byte)Math.min
+                (
+                    Math.min
+                    (
+                        m[x - 1][y] + 1,
+                        m[x][y - 1] + 1
+                    ),
+                    m[x - 1][y - 1] + (a.charAt(x - 1) == b.charAt(y - 1) ? 0 : 1)
+                );
+        }
+
+        for (int i = 0; i <= lnA - lnB; i++)
+        {
+            if ((i == 0 || a.charAt(i - 1) == ' ') && a.regionMatches(i, b, 0, lnB)) return m[lnA][lnB] / 4f;
+        }
+        for (int i = 0; i <= lnB - lnA; i++)
+        {
+            if ((i == 0 || b.charAt(i - 1) == ' ') && b.regionMatches(i, a, 0, lnA)) return m[lnA][lnB] / 4f;
+        }
+        return m[lnA][lnB] / (a.contains(b) || b.contains(a) ? 2f : 1f) * (lnA < 4 ? 1.2f : 1f) * (lnB < 4 ? 1.2f : 1f);
+    }
+
     /// Parses the expression and then evaluates it.
     public float evaluate(String expression, int level) { return levels[level].evaluate(expression); }
+
+    /// Selects unlocked blocks and then ranks them.
+    public Seq<Block> rank(String name) { return ranked.selectFrom(content.blocks(), polyblock::unlocked).sort(b -> distance(b.localizedName, name)); }
 
     // endregion
 }
