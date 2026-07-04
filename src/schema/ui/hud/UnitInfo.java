b6643667cacd031fdcf66e2b854cbf728711cdcf
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2026-07-04 17:28:40 +0300

    update the unit subfragment

--- src/schema/ui/hud/UnitInfo.java
+++ src/schema/ui/hud/UnitInfo.java
@@ -2,9 +2,11 @@ package schema.ui.hud;
 
 import arc.scene.*;
 import arc.scene.ui.layout.*;
+import arc.struct.*;
 import arc.util.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
+import schema.*;
 import schema.ui.*;
 import schema.ui.elements.*;
 
@@ -101,6 +103,36 @@ public class UnitInfo extends Table
                 pane.button(Icon.unitsSmall,   Style.ibc, () -> display[0] = false).checked(_ -> display[0] == false).size(24f);
             }
             ).growX().row();
+            cont.collapser(pane ->
+            {
+                final Bits effects = new Bits(content.statusEffects().size);
+                final float[] load = { 0f };
+
+                pane.update(() ->
+                {
+                    if (!player.dead() && !player.unit().statusBits().equals(effects) || units.paylodAbs() != load[0])
+                    {
+                        if (player.dead())
+                            effects.clear();
+                        else
+                            effects.set(player.unit().statusBits());
+
+                        load[0] = units.paylodAbs();
+
+                        pane.left().clear();
+                        content.statusEffects().each(e -> effects.get(e.id) && e.uiIcon.found(), e ->
+                        {
+                            pane.image(e.uiIcon).size(16f).scaling(Scaling.fit).tooltip(t -> t.background(Style.find("panel-x-shape")).margin(12f).label(() ->
+                            {
+                                return Tools.time(player.dead() ? 0f : player.unit().getDuration(e)).insert(0, " [light]").insert(0, e.localizedName);
+                            }
+                            ).style(Style.outline));
+                        });
+                        if (player.unit() instanceof Payloadc p) p.contentInfo(pane.table().grow().get(), 16f, 204f - pane.getPrefWidth());
+                    }
+                });
+            },
+            true, () -> !player.dead() && !player.unit().statusBits().isEmpty() || units.paylodAbs() > 0f).growX().row();
         }
         ).growX();
     }
